f8f70f607b0286564d6a5d3963bf45ef11318043
Chris <chris.haack@me.com>
2014-01-19 13:24:06 -0500

    run rails new and update gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HartlTutorial::Application.config.secret_key_base = '881f5f8f000f39b67991cd0701936c925d948cd0782ae8bd6aa82c8e326acd006a9cfe86553fbed92e2b912c03dcceb1331c8fad945ef4b4ef248dc74ac55290'
